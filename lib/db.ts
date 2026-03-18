f673eaa2eac75e3ad8343d3c169fdddaacd97a3d
Fran Ruiz <fran@peluquero.local>
2026-03-18 14:26:45 +0100

    fix: PrismaLibSql config directa

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,13 @@
-import { createClient } from '@libsql/client'
 import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 function createPrisma() {
-  const libsql = createClient({
+  const adapter = new PrismaLibSql({
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
   })
-  const adapter = new PrismaLibSql(libsql)
   return new PrismaClient({ adapter })
 }
 
