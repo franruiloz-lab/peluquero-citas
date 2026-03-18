fd0b687a0a809de2b8c804ed17352f22bbb8b07f
Fran Ruiz <fran@peluquero.local>
2026-03-18 14:25:55 +0100

    fix: PrismaLibSql nombre correcto

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@libsql/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
@@ -9,7 +9,7 @@ function createPrisma() {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
   })
-  const adapter = new PrismaLibSQL(libsql)
+  const adapter = new PrismaLibSql(libsql)
   return new PrismaClient({ adapter })
 }
 
